9ade47e1c0671b202839c0008fb6a6d4df3fa105
zeinaimam <zeina.moh.imam@gmail.com>
2026-05-03 01:08:56 +0300

    payment

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,8 @@ import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
+import paymentroutes from "./routes/payment.route.js";
+
 import { connectDB } from "./lib/db.js";
 dotenv.config();
 const app = express();
@@ -20,6 +22,8 @@ app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
+app.use("/api/payments", paymentroutes);
+ 
 app.listen(PORT, () => {
   console.log("server is running on http://localhost:" + PORT);
   connectDB();
