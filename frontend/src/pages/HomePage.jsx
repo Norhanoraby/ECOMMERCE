45b8f62795b575c060113a78f3c0606efd7e351e
caroll331 <C.Kerolle04803@student.aast.edu>
2026-05-04 17:05:21 +0300

    homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
+import CategoryItem from "../components/CategoryItem";
+
+const categories = [
+	{ href: "/jeans", name: "Jeans", imageUrl: "/jeans.jpg" },
+	{ href: "/blouse", name: "blouse", imageUrl: "/blouse.jpg" },
+	{ href: "/shoes", name: "Shoes", imageUrl: "/shoes.jpg" },
+	{ href: "/watches", name: "watches", imageUrl: "/watches.jpg" },
+	{ href: "/purse", name: "purse", imageUrl: "/purse1.jpg" },
+	{ href: "/dress", name: "dress", imageUrl: "/dress.jpg" },
+];
 
 function HomePage() {
   return (
   <div className="min-h-screen bg-[#F8F5F2] text-[#1A1A1A] px-6 py-20">
       <h1 className="text-4xl font-bold uppercase tracking-wide text-center">
-        Welcome to E-Commerce
+        Welcome to Sync
       </h1>
       <p className="text-center mt-4 text-[#6B6B6B]">
-        Discover premium products curated for you.
+       Where everything is in sync with your style.
       </p>
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4'>
+					{categories.map((category) => (
+						<CategoryItem category={category} key={category.name} />
+					))}
+				</div>
     </div>
   );
 };
