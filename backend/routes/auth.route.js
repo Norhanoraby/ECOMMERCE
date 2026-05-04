770c275090a765e666f54599bc8e8da1bd83e9ed
Norhanoraby <n.oraby06697@student.aast.edu>
2026-05-04 16:01:53 +0300

    edited theme

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,7 +1,7 @@
-import exopress from "express";
+import express from "express";
 import { signup, login, logout ,refreshToken} from "../controllers/auth.controller.js";
 
-const router = exopress.Router();
+const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
