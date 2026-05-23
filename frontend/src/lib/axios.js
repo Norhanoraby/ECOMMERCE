85f1ac8daf9d69a5c6bbc95133255ef1242a7269
Norhanoraby <n.oraby06697@student.aast.edu>
2026-05-23 18:00:16 +0300

    Fix deploy on frontend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: import.meta.mode === "development" ? "http://localhost:5000/api" : "/api",
-	withCredentials: true, // send cookies to the server
+	baseURL:
+		import.meta.env.MODE === "development"
+			? "http://localhost:5001/api"
+			: "https://ecommerce-production-e82f.up.railway.app/api",
+
+	withCredentials: true,
 });
 
 export default axiosInstance;
